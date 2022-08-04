61ae60b301f6de33ef04300910fb46b80a6fa9d2
Lee0308 <66457418+Lee0308@users.noreply.github.com>
2022-08-04 12:26:19 +0900

    JavaLearning

---
+++ 자바학습/Java/BasicType.java
@@ -0,0 +1,23 @@
+
+public class BasicType {
+
+	public static void main(String[] args) {
+		String toolName = "JDK";
+		char a = 'a';
+		byte b = -128;
+		short c = -32768;
+		int d = -2147483648; // - 2^31
+		long e = -9223372036854775808L; // - 2^63
+		float f = -2.408776F;
+		double g = -4.748679436904;
+		final double PI = 3.141592;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(PI);
+	}
+}
---
+++ 자바학습/Java/src/Literal.java
@@ -0,0 +1,12 @@
+
+public class Literal {
+
+	public static void main(String[] args) {
+		int a = 15;
+		int b = 015;
+		int c = 0x15;
+		int d = 0b0101;
+
+	}
+
+}
